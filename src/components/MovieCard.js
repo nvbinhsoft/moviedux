d29a3d39be38fac8897bed27909020a5b20fd983
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-16 18:37:05 +0700

    feat: create movies card component

---
+++ src/components/MovieCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import '../styles.css';
+
+export default function MovieCard({movie}) {
+    return (
+        <div className={'movie-card'} key={movie.id}>
+
+            <img src={`images/${movie.image}`} alt={movie.title}/>
+            <div className={'movie-card-info'}>
+                <h3 className={'movie-card-title'}>{movie.title}</h3>
+                <p className={'movie-card-genre'}>{movie.genre}</p>
+                <p className={'movie-card-rating'}>{movie.rating}</p>
+            </div>
+
+        </div>
+    )
+}
--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from "react";
 import '../styles.css';
+import MovieCard from "./MovieCard";
 
 export default function MoviesGrid() {
 
@@ -15,16 +16,7 @@ export default function MoviesGrid() {
         <div className={'movies-grid'}>
             {
                 movies.map(movie => (
-                    <div className={'movie-card'} key={movie.id}>
-                        <img src = {`images/${movie.image}`} alt={movie.title} />
-                        <div className={'movie-card-info'}>
-                            <h3 className={'movie-card-title'}>{movie.title}</h3>
-                            <p className={'movie-card-genre'}>{movie.genre}</p>
-                            <p className={'movie-card-rating'}>{movie.rating}</p>
-                        </div>
-
-                    </div>
-
+                    <MovieCard movie={movie} key = {movie.id}/>
                 ))
             }
         </div>
