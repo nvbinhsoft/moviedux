a21245dded1f1b19f46d2d0fab05583376e08198
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-17 18:14:58 +0700

    feat: add search function

--- src/components/MoviesGrid.js
+++ src/components/MoviesGrid.js
@@ -5,6 +5,7 @@ import MovieCard from "./MovieCard";
 export default function MoviesGrid() {
 
     const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState("");
 
     useEffect(() => {
         fetch("movies.json")
@@ -12,13 +13,33 @@ export default function MoviesGrid() {
             .then(data => setMovies(data))
     }, [])
 
+    const handleSearchChange = (event) => {
+        setSearchTerm(event.target.value);
+    }
+
+    const filteredMovies = movies.filter(movie => {
+        return movie.title.toLowerCase().includes(searchTerm.toLowerCase());
+    })
+
     return (
-        <div className={'movies-grid'}>
-            {
-                movies.map(movie => (
-                    <MovieCard movie={movie} key = {movie.id}/>
-                ))
-            }
+        <div>
+            <input
+            type={'text'}
+            placeholder={'Search movies'}
+            className={'search-input'}
+            value={searchTerm}
+            onChange={handleSearchChange}
+            />
+
+            <div className={'movies-grid'}>
+                {
+                    filteredMovies.map(movie => (
+                        <MovieCard movie={movie} key={movie.id}/>
+                    ))
+                }
+            </div>
+
         </div>
+
     )
 }
