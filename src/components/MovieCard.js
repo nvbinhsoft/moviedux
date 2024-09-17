2a53fb51e61c3109b1f682037e8ee165bbca67fa
nvbinhsoft <nvbinhsoft@gmail.com>
2024-09-17 17:59:31 +0700

    feat: add error handle and add rating color

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,14 +2,29 @@ import React from "react";
 import '../styles.css';
 
 export default function MovieCard({movie}) {
+
+    const handleError = (event) => {
+        event.target.src = 'images/default.jpg';
+    }
+
+    const getRatingClass = (rating) => {
+        if (rating >= 7) {
+            return 'rating-good';
+        } else if (rating >= 4) {
+            return 'rating-ok';
+        } else {
+            return 'rating-bad';
+        }
+    }
+
     return (
         <div className={'movie-card'} key={movie.id}>
 
-            <img src={`images/${movie.image}`} alt={movie.title}/>
+            <img src={`images/${movie.image}`} alt={movie.title} onError={handleError}/>
             <div className={'movie-card-info'}>
                 <h3 className={'movie-card-title'}>{movie.title}</h3>
                 <p className={'movie-card-genre'}>{movie.genre}</p>
-                <p className={'movie-card-rating'}>{movie.rating}</p>
+                <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>{movie.rating}</p>
             </div>
 
         </div>
